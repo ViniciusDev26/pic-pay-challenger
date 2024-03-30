b864389318c3c3013a145f7f1aa9a84bfb0678de
Carlos Vinicius <viniciusdev.26@gmail.com>
2024-03-30 02:22:53 -0300

    ✨ create wallet abstraction

---
+++ src/bank/entities/wallet.ts
@@ -0,0 +1,29 @@
+import { Entity } from '../../core/entities/Entity'
+
+interface WalletProps {
+  balance: number
+  userId: string
+}
+
+export class Wallet extends Entity<WalletProps> {
+  get balance (): number {
+    return this.props.balance
+  }
+
+  get userId (): string {
+    return this.props.userId
+  }
+
+  withdraw (amount: number): void {
+    if (amount < 0) throw new Error('Invalid amount')
+    if (this.props.balance < amount) throw new Error('Insufficient funds')
+
+    this.props.balance -= amount
+  }
+
+  deposit (amount: number): void {
+    if (amount < 0) throw new Error('Invalid amount')
+
+    this.props.balance += amount
+  }
+}
